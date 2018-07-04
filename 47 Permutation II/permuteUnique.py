43d83ef7348984d1546c6d36bcbe870fab815ced
Terry Ye <yeyunong@hotmail.com>
2018-07-05 02:09:02 +0800

    Update permuteUnique.py

--- 47 Permutation II/permuteUnique.py
+++ 47 Permutation II/permuteUnique.py
@@ -1,1 +1,21 @@
-
+class Solution:
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if not nums:
+            return [[]]
+        result = []
+        index = 0
+        visited = []
+        for index in range(len(nums)):
+            temp = [nums[index]]
+            if index > 0 and nums[index] in visited:
+                continue
+            temp_result = self.permuteUnique(nums[:index]+nums[index+1:])
+            for part in temp_result:
+                tp = temp+part
+                result.append(tp)
+            visited.append(nums[index])
+        return result
