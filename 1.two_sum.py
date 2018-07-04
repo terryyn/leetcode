cc0544a12c7392d048e644bb5d3388c541d33138
Terry Ye <yeyunong@hotmail.com>
2018-07-05 01:54:07 +0800

    Update 1.two_sum.py

--- 1.two_sum.py
+++ 1.two_sum.py
@@ -1,1 +1,24 @@
+"""
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+"""
+
+def twoSum(self, nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    table = {}
+    number  = len(nums)
+    for i in range(0,number):
+        key = target - nums[i]
+        if key in table:
+            return [table[key],i]
+        else:
+            table[nums[i]] = i
+    return  
+  
+ 
+#a simple hash table algorithm, here I use one-pass but two-pass is also ok
